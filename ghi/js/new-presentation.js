12e6e2b7ea138a651711973716b954f1633094d8
James Hoffman <jamesvhoffman@outlook.com>
2022-10-12 18:50:27 -0400

    Completed stretch goal for creating presentations

---
+++ ghi/js/new-presentation.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', async () => {
+  const url = 'http://localhost:8000/api/conferences/';
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    const selectTag = document.querySelector('#conference');
+
+    for (let conference of data.conferences) {
+      const option = document.createElement('option');
+      option.value = conference.href;
+      option.innerHTML = conference.name;
+      selectTag.appendChild(option);
+    }
+  }
+
+  const formTag = document.getElementById('create-presentation-form');
+  formTag.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const formData = Object.fromEntries(new FormData(formTag));
+    const conferenceHREF = formData.conference;
+    delete formData.conference;
+    const json = JSON.stringify(formData);
+
+    const locationUrl = `http://localhost:8000${conferenceHREF}presentations/`;
+    const fetchConfig = {
+      method: "post",
+      body: json,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch(locationUrl, fetchConfig);
+    if (response.ok) {
+      formTag.reset();
+    }
+  });
+});
