cbff58cafc03b675957b6362e6e4aacf484c65ee
James Hoffman <jamesvhoffman@outlook.com>
2022-10-13 14:28:50 -0400

    Completed form for attendee sign up

--- ghi/js/attend-conference.js
+++ ghi/js/attend-conference.js
@@ -17,4 +17,27 @@ window.addEventListener('DOMContentLoaded', async () => {
     loadingTag.classList.add('d-none');
     selectTag.classList.remove('d-none');
   }
+
+  const successTag = document.getElementById('success-message');
+  const formTag = document.getElementById('create-attendee-form');
+  formTag.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const formData = Object.fromEntries(new FormData(formTag));
+    const json = JSON.stringify(formData);
+
+    const locationUrl = `http://localhost:8001/api/attendees/`;
+    const fetchConfig = {
+      method: "post",
+      body: json,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch(locationUrl, fetchConfig);
+    if (response.ok) {
+      formTag.classList.add('d-none');
+      successTag.classList.remove('d-none');
+    }
+  });
 });
