919429c119e8cbedb941235e0e4562e4c29cf696
James Hoffman <jamesvhoffman@outlook.com>
2022-10-14 15:55:17 -0400

    Updated to display all attendees along with conferences

--- ghi/attendees/src/index.js
+++ ghi/attendees/src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const loadAttendees = async () => {
+  const response = await fetch('http://localhost:8001/api/attendees/');
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data);
+    root.render(
+      <React.StrictMode>
+        <App attendees={data.attendees} />
+      </React.StrictMode>
+    );
+  } else {
+    console.error(response);
+  }
+};
+loadAttendees();
+
+// ========================================
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,9 +33,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-const loadAttendees = async () => {
-  const response = await fetch('http://localhost:8001/api/attendees/');
-  console.log(response);
-};
-loadAttendees();
