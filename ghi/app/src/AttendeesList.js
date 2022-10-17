5b8f8476867df4abdae0fb9da094d8939a880a84
James Hoffman <jamesvhoffman@outlook.com>
2022-10-17 13:26:15 -0400

    Removed container div

--- ghi/app/src/AttendeesList.js
+++ ghi/app/src/AttendeesList.js
@@ -1,26 +1,24 @@
 const AttendeesList = (props) => {
   return (
-    <div className="container">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Conference</th>
-          </tr>
-        </thead>
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Conference</th>
+        </tr>
+      </thead>
 
-        <tbody>
-          {props.attendees.map(attendee => {
-            return (
-              <tr key={attendee.href}>
-                <td>{attendee.name}</td>
-                <td>{attendee.conference}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+      <tbody>
+        {props.attendees.map(attendee => {
+          return (
+            <tr key={attendee.href}>
+              <td>{attendee.name}</td>
+              <td>{attendee.conference}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
