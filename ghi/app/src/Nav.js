3a5a783da38ed0c2b22b876e8f7f3277c8e1969d
James Hoffman <jamesvhoffman@outlook.com>
2022-10-17 12:53:57 -0400

    Small refactoring using destructuring

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,9 +1,10 @@
 const Nav = (props) => {
+  const { rootHost, locationPerm, conferencePerm } = props.navItems;
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href={props.navItems.rootHost}>Conference GO!</a>
+          <a className="navbar-brand" href={rootHost}>Conference GO!</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -11,16 +12,16 @@ const Nav = (props) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href={props.navItems.rootHost}>Home</a>
+                <a className="nav-link active" aria-current="page" href={rootHost}>Home</a>
               </li>
               <li className="nav-item">
-                <a className={"nav-link" + (props.navItems.locationPerm ? "" : " d-none")} aria-current="page" href={props.navItems.rootHost + "new-location.html"}>New location</a>
+                <a className={"nav-link" + (locationPerm ? "" : " d-none")} aria-current="page" href={rootHost + "new-location.html"}>New location</a>
               </li>
               <li className="nav-item">
-                <a className={"nav-link" + (props.navItems.conferencePerm ? "" : " d-none")} aria-current="page" href={props.navItems.rootHost + "new-conference.html"}>New conference</a>
+                <a className={"nav-link" + (conferencePerm ? "" : " d-none")} aria-current="page" href={rootHost + "new-conference.html"}>New conference</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href={props.navItems.rootHost + "new-presentation.html"}>New presentation</a>
+                <a className="nav-link" aria-current="page" href={rootHost + "new-presentation.html"}>New presentation</a>
               </li>
             </ul>
           </div>
