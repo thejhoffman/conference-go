7411345b99449075966ed1ccfd94f6d7a707d70c
James Hoffman <jamesvhoffman@outlook.com>
2022-10-17 18:58:43 -0400

    Comment added for SPA staging

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Nav from './Nav';
 import AttendeesList from './AttendeesList';
+// import LocationForm from './LocationForm';
+// import ConferenceForm from './ConferenceForm';
+// import AttendConferenceForm from './AttendConferenceForm';
+// import PresentationForm from './PresentationForm';
 
 const App = (props) => {
   if (props.attendees === undefined)
@@ -9,6 +13,10 @@ const App = (props) => {
     <React.Fragment>
       <Nav navItems={props.navItems} />
       <div className="container">
+        {/* <PresentationForm /> */}
+        {/* <AttendConferenceForm /> */}
+        {/* <ConferenceForm /> */}
+        {/* <LocationForm /> */}
         <AttendeesList attendees={props.attendees} />
       </div>
     </React.Fragment>
