6b8cfe9a168a82009c3d7cdf48c21a2c1a70de5c
James Hoffman <jamesvhoffman@outlook.com>
2022-10-18 16:55:54 -0400

    Added MainPage component and removed container div

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Nav from './Nav';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
 import AttendeesList from './AttendeesList';
+import AttendConferenceForm from './AttendConferenceForm';
 import LocationForm from './LocationForm';
 import ConferenceForm from './ConferenceForm';
-import AttendConferenceForm from './AttendConferenceForm';
 import PresentationForm from './PresentationForm';
 
 const App = (props) => {
@@ -13,22 +14,22 @@ const App = (props) => {
   return (
     <BrowserRouter>
       <Nav navPerms={props.navPerms} />
-      <div className="container">
-        <Routes>
-          <Route path="attendees" element={<AttendeesList attendees={props.attendees} />}>
-            <Route path="new" element={<AttendConferenceForm />} />
-          </Route>
-          <Route path="locations">
-            <Route path="new" element={<LocationForm />} />
-          </Route>
-          <Route path="conferences">
-            <Route path="new" element={<ConferenceForm />} />
-          </Route>
-          <Route path="presentations">
-            <Route path="new" element={<PresentationForm />} />
-          </Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="attendees">
+          <Route index element={<AttendeesList attendees={props.attendees} />} />
+          <Route path="new" element={<AttendConferenceForm />} />
+        </Route>
+        <Route path="locations">
+          <Route path="new" element={<LocationForm />} />
+        </Route>
+        <Route path="conferences">
+          <Route path="new" element={<ConferenceForm />} />
+        </Route>
+        <Route path="presentations">
+          <Route path="new" element={<PresentationForm />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
