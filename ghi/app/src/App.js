811a111bcbf88cb728a98798ad6ba859c4def7ec
James Hoffman <jamesvhoffman@outlook.com>
2022-10-18 14:25:34 -0400

    Updated Nav and App using routes

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import Nav from './Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AttendeesList from './AttendeesList';
-// import LocationForm from './LocationForm';
-// import ConferenceForm from './ConferenceForm';
-// import AttendConferenceForm from './AttendConferenceForm';
-// import PresentationForm from './PresentationForm';
+import LocationForm from './LocationForm';
+import ConferenceForm from './ConferenceForm';
+import AttendConferenceForm from './AttendConferenceForm';
+import PresentationForm from './PresentationForm';
 
 const App = (props) => {
   if (props.attendees === undefined)
     return null;
   return (
-    <React.Fragment>
+    <BrowserRouter>
       <Nav navItems={props.navItems} />
       <div className="container">
-        {/* <PresentationForm /> */}
-        {/* <AttendConferenceForm /> */}
-        {/* <ConferenceForm /> */}
-        {/* <LocationForm /> */}
-        <AttendeesList attendees={props.attendees} />
+        <Routes>
+          <Route path="attendees" element={<AttendeesList attendees={props.attendees} />}>
+            <Route path="new" element={<AttendConferenceForm />} />
+          </Route>
+          <Route path="locations">
+            <Route path="new" element={<LocationForm />} />
+          </Route>
+          <Route path="conferences">
+            <Route path="new" element={<ConferenceForm />} />
+          </Route>
+          <Route path="presentations">
+            <Route path="new" element={<PresentationForm />} />
+          </Route>
+        </Routes>
       </div>
-    </React.Fragment>
+    </BrowserRouter>
   );
 };
 
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,10 +1,12 @@
+import { NavLink } from "react-router-dom";
+
 const Nav = (props) => {
   const { rootHost, locationPerm, conferencePerm } = props.navItems;
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href={rootHost}>Conference GO!</a>
+          <NavLink to="/" className="navbar-brand">Conference GO!</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -12,16 +14,16 @@ const Nav = (props) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href={rootHost}>Home</a>
+                <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
               </li>
               <li className="nav-item">
-                <a className={"nav-link" + (locationPerm ? "" : " d-none")} aria-current="page" href={rootHost + "new-location.html"}>New location</a>
+                <NavLink to="/locations/new" className={"nav-link" + (locationPerm ? "" : " d-none")} aria-current="page">New location</NavLink>
               </li>
               <li className="nav-item">
-                <a className={"nav-link" + (conferencePerm ? "" : " d-none")} aria-current="page" href={rootHost + "new-conference.html"}>New conference</a>
+                <NavLink to="/conferences/new" className={"nav-link" + (conferencePerm ? "" : " d-none")} aria-current="page">New conference</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href={rootHost + "new-presentation.html"}>New presentation</a>
+                <NavLink to="/presentations/new" className="nav-link" aria-current="page">New presentation</NavLink>
               </li>
             </ul>
           </div>
